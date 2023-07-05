7872cf963ba39dddeb629e784ce814611d04124e
armangencer <93614496+armangencer@users.noreply.github.com>
2023-07-05 15:35:05 +0300

    final

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 10), () {
+    Timer(const Duration(seconds: 5), () {
       Navigator.pushNamed(context, '/homePage');
     });
   }
